cc125d143ff8d584efe480ddeb898db68ac9acd4
poppy22 <carmengpopa13@gmail.com>
2023-11-14 09:41:24 +0300

    MPC experiments

--- experiments_mpc/helpers/mpc/sparse_linreg_damgard.py
+++ experiments_mpc/helpers/mpc/sparse_linreg_damgard.py
@@ -29,12 +29,9 @@ def reveal_and_print(x, info_text=''):
 # returns an array of the indices that should be made zero (i.e. the smallest n - k values)
 # makes use of the bits_to_cut for efficiency
 
-def get_min_indices(x, k, bits_to_cut):
-    x_copy = x.clone() # to avoid cutting bits on the real input, which is returned at the end
-
-    n = len(x)
-    if k == 0 or k == n:
-        return x # no change needed
+def get_indices(x, noise, k, d, bits_to_cut=0):
+    index_k_argmax = torch.zeros(d)
+    x_copy = x.clone() # to avoid cutting bits on the real input
     
     # preparing for argmax:
     # cutting c bits for efficiency reasons
@@ -42,24 +39,25 @@ def get_min_indices(x, k, bits_to_cut):
         x_copy = x_copy.div(bits_to_cut) # removing the last c bits from every share
 
     x_copy_abs = x_copy.abs()
-    max_value = x_copy_abs.max()
-    argmin_index_rev = []
-    k = n - k
+    min_value = x_copy_abs.min()
 
-    for _ in range(k):
-        min_index = x_copy_abs.argmin(one_hot=False)
+    for i in range(k):
+        x_copy_abs_2 = x_copy_abs.clone()
+        noise_iteration_shared = crypten.cryptensor(noise[i], ptype=crypten.mpc.arithmetic)
+        x_copy_abs_2 = x_copy_abs_2 + noise_iteration_shared
 
-        min_index_rev = min_index.get_plain_text()
-        x_copy_abs[min_index_rev] = max_value # set to the maximum value to avoid finding the same index in the next iteration
+        max_index = x_copy_abs_2.argmax(one_hot=False)
+        max_index_rev = max_index.get_plain_text()
+        index_k_argmax[max_index_rev] = 1
 
-        argmin_index_rev.append(min_index_rev)
+        x_copy_abs[max_index_rev] = min_value # set to the maximum value to avoid finding the same index in the next iteration
 
-    return argmin_index_rev
+    return index_k_argmax
 
 
 # ================== Implemented an updated distributed_noise_and_round from Damgard's paper ==================
 @mpc.run_multiprocess(world_size=2)
-def distributed_noise_and_round(nabla_input, noise_output, theta_prev_input, n, p, k=2, a=64, c=0, eta=0.1, t=0):
+def distributed_noise_and_round(nabla_input, noise_iterations, noise_output, theta_prev_input, n, d, k=2, a=64, c=0, eta=0.1, t=0):
     nabla = crypten.cryptensor(nabla_input, ptype=crypten.mpc.arithmetic)
     theta_prev = crypten.cryptensor(theta_prev_input, ptype=crypten.mpc.arithmetic)
     noise_output_shared = crypten.cryptensor(noise_output, ptype=crypten.mpc.arithmetic)
@@ -78,12 +76,13 @@ def distributed_noise_and_round(nabla_input, noise_output, theta_prev_input, n, 
     # Ring 2^a ==> TODO if it works, move inside get_min_indices
     # theta_trunc = mod_2a(theta_trunc, a)
 
-    min_indices = get_min_indices(theta_t, k, c)
+    indices = get_indices(theta_t, noise_iterations, k, d, c)
 
     # make k-sparse
-    for i in min_indices:
-        zero = crypten.cryptensor(0, ptype=crypten.mpc.arithmetic)
-        theta_t[i] = zero
+    for i in range(d):
+        if indices[i] == 0:
+            zero = crypten.cryptensor(0, ptype=crypten.mpc.arithmetic)
+            theta_t[i] = zero
 
     theta_t = theta_t + noise_output_shared
 
@@ -100,11 +99,30 @@ def generate_laplace_noise(lmb, ebs, dta, s, d, n_servers):
     return full_noise
 
 
+def generate_noise(noise_params, T_max, k, p, n_servers=1):
+    lmb, ebs, dlt, dp = noise_params
+    # TODO: ebs, dlt = ebs / T_max, dlt / T_max
+
+    if dp == 'no_noise':
+        return [torch.zeros(p) for _ in range(k)], torch.zeros(p)
+
+    if dp == 'noise_output':
+        noise_selection_output = torch.tensor(generate_laplace_noise(lmb, ebs, dlt, k, p, n_servers))
+        return [torch.zeros(p) for _ in range(k)], noise_selection_output
+
+    # if noise == 'noise_output_and_per_it'
+    noise_selection_iterations = []
+    for _ in range(k):
+        iteration_noise = torch.tensor(generate_laplace_noise(lmb, ebs, dlt, k, p, n_servers))
+        noise_selection_iterations.append(iteration_noise)
+    noise_selection_output = torch.tensor(generate_laplace_noise(lmb, ebs, dlt, k, p, n_servers))
+    return noise_selection_iterations, noise_selection_output
+
+
 def solve(data, noise_params, k, T_max, a=64, c=0, eta=0.1, eps=0.001):
     x_input, y_input, theta_star = data # unpack data
     n = len(x_input)
     p = len(x_input[0])
-    lmb, ebs, dlt = noise_params
 
     theta_prev = torch.zeros(p)
     nabla = torch.zeros((n, p))
@@ -120,7 +138,7 @@ def solve(data, noise_params, k, T_max, a=64, c=0, eta=0.1, eps=0.001):
         if t % 10 == 0:
             print(t)
 
-        noise_output = torch.tensor(generate_laplace_noise(lmb, ebs / T_max, dlt / T_max, k, p, 1))
+        noise_selection_iterations, noise_selection_output = generate_noise(noise_params, T_max, k, p, 1)
 
         nabla = torch.zeros((n, p))
         nabla = nabla.double()
@@ -132,7 +150,7 @@ def solve(data, noise_params, k, T_max, a=64, c=0, eta=0.1, eps=0.001):
             var2 = x_input[i].transpose(0, -1)
             nabla[i] = var2.mul(var1)
 
-        result = distributed_noise_and_round(nabla, noise_output, theta_prev, n, p, k, a, c, eta, t)
+        result = distributed_noise_and_round(nabla, noise_selection_iterations, noise_selection_output, theta_prev, n, p, k, a, c, eta, t)
         theta_t = result[0] # just from the first party
 
         e = torch.norm(theta_t - theta_star, p=2)
